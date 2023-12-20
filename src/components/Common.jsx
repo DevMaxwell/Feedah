4ad443c821c407c31fb7afc66041af69b97c2e68
DevMaxwell <anoliefomaxwell2@gmail.com>
2023-12-20 14:31:10 -0800

    initial push

--- src/components/Common.jsx
+++ src/components/Common.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Common() {
+  useEffect(() => {
+    getCommon();
+  }, []);
+
   const getCommon = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apikey=${process.env.FEEDAH_API_KEY}`
+      `https://api.spoonacular.com/recipes/random?apikey=${process.env.FEEDAH_API_KEY}&number=9`
     );
     const data = await api.json();
     console.log(data);
