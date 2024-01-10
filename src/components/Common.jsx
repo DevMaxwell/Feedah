c8e222598141bde7953fd3361f0c39b6b5021123
DevMaxwell <anoliefomaxwell2@gmail.com>
2024-01-10 10:57:29 -0800

    feat: fetch vegies data vegies4U / trending recipes

--- src/components/Common.jsx
+++ src/components/Common.jsx
@@ -1,18 +1,100 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Common() {
+  const [common, setCommon] = useState([]);
+
   useEffect(() => {
     getCommon();
   }, []);
 
+  //https://api.spoonacular.com/recipes/random?apiKey=52efd6e2bab54c2b9f2a8b8845af7c75&number=9
   const getCommon = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apikey=${process.env.FEEDAH_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    console.log(data);
+    const check = localStorage.getItem("common");
+
+    if (check) {
+      setCommon(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=00040191b1e84a6f9c1f2c1309a1b6c2&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("common", JSON.stringify(data.recipes));
+      setCommon(data.recipes);
+      console.log(data);
+    }
   };
-  return <div></div>;
+  return (
+    <div>
+      <Wrapped>
+        <h4>Trending picks</h4>
+        <Splide
+          options={{
+            perPage: 3,
+            //arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {common.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapped>
+    </div>
+  );
 }
 
 export default Common;
+
+const Wrapped = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 35rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+    postion: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    postion: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0, 0.5));
+`;
--- src/components/Vegie.jsx
+++ src/components/Vegie.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Vegie() {
+  const [vegie, setVegie] = useState([]);
+
+  useEffect(() => {
+    getVegie();
+  }, []);
+
+  //https://api.spoonacular.com/recipes/random?apiKey=52efd6e2bab54c2b9f2a8b8845af7c75&number=9
+  const getVegie = async () => {
+    const check = localStorage.getItem("vegie");
+
+    if (check) {
+      setVegie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.FEEDAH_API_KEY}&number=9&tags=vegetarian`
+      );
+      const data = await api.json();
+      localStorage.setItem("vegie", JSON.stringify(data.recipes));
+      setVegie(data.recipes);
+      console.log(data);
+    }
+  };
   return (
     <div>
-      
+      <Wrapped>
+        <h4>Vegies 4U</h4>
+        <Splide
+          options={{
+            perPage: 3,
+            //arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {vegie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapped>
     </div>
-  )
+  );
 }
 
-export default Vegie
+export default Vegie;
+
+const Wrapped = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 35rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+    postion: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    postion: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+  }
+`;
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0, 0.5));
+`;
